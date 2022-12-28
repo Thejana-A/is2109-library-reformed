7b4fd7b919facf4d9a9079acc4087ddfb7ff9b2e
Daweendri <83944867+Daweendri@users.noreply.github.com>
2022-12-28 16:39:44 +0530

    Update admin_delete_member.php

--- admin_delete_member.php
+++ admin_delete_member.php
@@ -27,7 +27,7 @@
         mysqli_close($conn);
     ?>
 </head>
-<body>
+<body style="background-image: url('icon/admin_home.jpg');background-repeat:no-repeat;background-attachment:fixed;background-size:cover;">
     <div class="container">
         <div class="top">
             <img src="./icon/logo.jpg" style="float:left;width:80px;">
@@ -39,12 +39,14 @@
             <a href="admin_edit_self_profile.php"><?php echo $_SESSION["username"]; ?></a> 
         </div>
         <div class="nav">
-            <center>
-                <a href="admin_books.php">Books</a>
-                <a href="admin_members.php">Members</a>
-                <a href="admin_borrowings.php">Borrowings</a>
-            </center>
-        </div>
+            <div class="nav-btn">
+                <center>
+                   <a href="admin_books.php">Books</a>
+                   <a href="admin_members.php">Members</a>
+                   <a href="admin_borrowings.php">Borrowings</a>
+                </center>
+            </div>
+        </div> <br>
 
         <center>
             <div class="form-box">
