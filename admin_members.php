61d9841c13c59699b50c5434c4f8edb321ac86db
Daweendri <83944867+Daweendri@users.noreply.github.com>
2022-12-28 16:19:00 +0530

    Update admin_members.php

--- admin_members.php
+++ admin_members.php
@@ -5,7 +5,7 @@
     <title>Admin books</title>
     <link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
-<body>
+<body style="background-image: url('icon/admin_home.jpg');background-repeat:no-repeat;background-attachment:fixed;background-size:cover;">
     <div class="container">
         <div class="top">
             <img src="./icon/logo.jpg" style="float:left;width:80px;">
@@ -17,12 +17,14 @@
             <a href="admin_edit_self_profile.php"><?php echo $_SESSION["username"]; ?></a> 
         </div>
         <div class="nav">
-            <center>
-                <a href="admin_books.php">Books</a>
-                <a href="admin_members.php">Members</a>
-                <a href="admin_borrowings.php">Borrowings</a>
-            </center>
-        </div>
+            <div class="nav-btn">
+                <center>
+                   <a href="admin_books.php">Books</a>
+                   <a href="admin_members.php">Members</a>
+                   <a href="admin_borrowings.php">Borrowings</a>
+                </center>
+            </div>
+        </div> <br>
         <center>
             <br>
             <div class="form-box">
