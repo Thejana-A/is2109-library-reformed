0ee5c9c6ae5bd72879ec4804f8cbb627d3463639
Sewmi <sewmithimaya248@gmail.com>
2022-12-29 00:52:36 +0530

    login validations

--- login.php
+++ login.php
@@ -5,19 +5,35 @@
     $_SESSION["email"] = ""; 
     $_SESSION["userID"] = ""; 
     $_SESSION["username"] = ""; 
+
+    if (isset($_SESSION["locked"])) {
+        $difference = time() - $_SESSION["locked"];
+        if ($difference > 3) {
+            unset($_SESSION["locked"]);
+            unset($_SESSION["attempts"]);
+        }
+}
 ?>
+
 <html>
+
 <head>
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
-<body style="background-image: url('icon/common.jpg');background-repeat:no-repeat;background-attachment:fixed;background-size:cover;">
+
+<body
+    style="background-image: url('icon/common.jpg');background-repeat:no-repeat;background-attachment:fixed;background-size:cover;">
     <center><br><br><br><br><br><br>
         <div class="login-box">
             <form method="post" action="php/login.php">
                 <table>
                     <tr>
-                        <td><center><h3>Login</h3></center></td>
+                        <td>
+                            <center>
+                                <h3>Login</h3>
+                            </center>
+                        </td>
                     </tr>
                     <tr>
                         <td>User email</td>
@@ -32,11 +48,30 @@
                         <td><input type="password" name="password" required /></td>
                     </tr>
                     <tr>
-                        <td><center><input type="submit" value="Login" id="submit-button" /></center></td>
+                        <td><?php
+                                if (!isset($_SESSION["error"])) {
+                                    $_SESSION["error"] = null;
+                                } else {
+                                    echo "<p class='text' style='color:red;text-align:center;padding:4px;'>";
+                                    echo $_SESSION["error"];
+                                    echo "</p>";
+                                    unset($_SESSION["error"]);
+                                }
+                                if (!isset($_SESSION["attempts"])) {
+                                    $_SESSION["attempts"] = null;
+                                }
+                                if ($_SESSION["attempts"] > 3) {
+                                    $_SESSION["locked"] = time();
+                                    echo "<div class='text' style='color:red;text-align:center;padding:1px;margin-top:2px;'>Try again after 3 seconds</div>";
+                                } else { ?>
+                            <center><input type="submit" value="Login" id="submit-button" /></center>
+                        </td>
+                        <?php } ?>
                     </tr>
                 </table>
             </form>
         </div>
     </center>
 </body>
+
 </html>
--- php/login.php
+++ php/login.php
@@ -1,56 +1,68 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "is2109_library_reformed";
+session_start();
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error){
-        die("Connection failed: " . $conn->connect_error);
-    }
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "is2109_library_reformed";
 
-    $sql = "SELECT * FROM user WHERE email = '".$_POST["email"]."';";
-    $result = mysqli_query($conn, $sql);
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-    if (mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
-        if($row["password"] == md5($_POST["password"])){
-            if($row["email_verification"] == 1){
-                if($row["active_status"] == "enable"){
-                    session_start();
-                    $_SESSION["email"] = $row["email"]; 
-                    $_SESSION["userID"] = $row["userID"]; 
-                    $_SESSION["username"] = $row["first_name"]." ".$row["last_name"]; 
-                    if($row["userID"] == 1){
-                        header("location: http://localhost/is2109-library-reformed/admin_home.php");
-                    }else{
-                        header("location: http://localhost/is2109-library-reformed/member_home.php");
-                    }
-                    
-                    date_default_timezone_set("Asia/Calcutta");
-                    $userID = $_SESSION["userID"];
-	                $timestamp = date("Y-m-d h:i:s A");
-	                echo file_put_contents("login_log.txt","Log in by $userID - $timestamp \n",FILE_APPEND);
-                }else{
-                    echo "<div style='background-color:#a8a8ec;border-radius:3px;padding:5px;'>";
-                    echo "<center>Sorry! Your account is inactive.</center>";
-                    echo "</div>";
+$sql = "SELECT * FROM user WHERE email = '" . $_POST["email"] . "';";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    if ($row["password"] == md5($_POST["password"])) {
+        if ($row["email_verification"] == 1) {
+            if ($row["active_status"] == "enable") {
+                session_start();
+                $_SESSION["email"] = $row["email"];
+                $_SESSION["userID"] = $row["userID"];
+                $_SESSION["username"] = $row["first_name"] . " " . $row["last_name"];
+                if ($row["userID"] == 1) {
+                    header("location: http://localhost/is2109-library-reformed/admin_home.php");
+                } else {
+                    header("location: http://localhost/is2109-library-reformed/member_home.php");
                 }
-            }else{
+
+                date_default_timezone_set("Asia/Calcutta");
+                $userID = $_SESSION["userID"];
+                $timestamp = date("Y-m-d h:i:s A");
+                echo file_put_contents("login_log.txt", "Log in by $userID - $timestamp \n", FILE_APPEND);
+            } else {
                 echo "<div style='background-color:#a8a8ec;border-radius:3px;padding:5px;'>";
-                echo "<center>Sorry! Your email isn't verified.</center>";
+                echo "<center>Sorry! Your account is inactive.</center>";
                 echo "</div>";
-            }                   
-            
-        }else{
+            }
+        } else {
             echo "<div style='background-color:#a8a8ec;border-radius:3px;padding:5px;'>";
-            echo "<center>Sorry! Credentials are invalid</center>";
+            echo "<center>Sorry! Your email isn't verified.</center>";
             echo "</div>";
         }
-        
+
     } else {
-        echo "0 results";
+        // echo "<div style='background-color:#a8a8ec;border-radius:3px;padding:5px;'>";
+        // echo "<center>Sorry! Credentials are invalid</center>";
+        // echo "</div>";
+        $_SESSION["attempts"] += 1;
+        $_SESSION["error"] = "Sorry! Credentials are invalid";
+        echo "<script type='text/javascript'>
+            window.location.replace('../login.php')
+            </script>";
+
     }
 
-    $conn->close(); 
-?>
+} else {
+    $_SESSION["attempts"] += 1;
+    $_SESSION["error"] = "Sorry! Credentials are invalid";
+    echo "<script type='text/javascript'>
+        window.location.replace('../login.php');
+        </script>";
+
+}
+
+$conn->close();
