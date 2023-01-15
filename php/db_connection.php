1e32365621ee252ddd05248f4d2f5c366f8730e5
thejanaakmeemana <thejanaakmeemana@gmail.com>
2023-01-15 14:00:59 +0530

    commit 13

--- php/db_connection.php
+++ php/db_connection.php
@@ -1,11 +1,15 @@
 <?php
-    $servername = "localhost";
-    //$username = "root";
-    $user = "root";
+    /*$servername = "localhost";
+    $username = "root";
     $password = "";
     $dbname = "is2109_library_reformed";
 
-    $conn = mysqli_connect($servername, $user, $password, $dbname);
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn){
+        die("Connection failed: ".mysqli_connect_error());
+    } */
+    $db_params = parse_ini_file( dirname(__FILE__).'/db_params.ini', false );
+    $conn = mysqli_connect($db_params['servername'], $db_params['username'], $db_params['password'], $db_params['dbname']);
     if (!$conn){
         die("Connection failed: ".mysqli_connect_error());
     }
